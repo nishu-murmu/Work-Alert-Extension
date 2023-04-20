918f3e06c85ab0148c1488d1f0619659aaf7b96e
nishu-murmu <nishumurmu017@gmail.com>
2023-04-20 19:18:12 +0530

    feat: validating valid url

--- src/options/components/commonComponent/AddKeyWord.tsx
+++ src/options/components/commonComponent/AddKeyWord.tsx
@@ -6,17 +6,23 @@ import useOpJobs from '../../../customHooks/use-option-jobs'
 const AddKeyWordSection: React.FC = () => {
   const [keyword, setKeyword] = useState<string>('')
   const [rssLink, setRssLink] = useState<string>('')
+  const [valid, setIsValid] = useState<boolean>(true)
   const [jobs, setJobs] = useRecoilState(jobsState)
   const [allJobs, setAllJobs] = useRecoilState(allJobsState)
   const [isEmptyFields, setIsEmptyFields] = useRecoilState(isEmpty)
   const { setLocalJobs, getLocalJobs, setLocalKeywords } = useOpJobs()
 
   const submitHandler = async (keyword: string, rssLink: string) => {
+    const regex = new RegExp('^https://www.upwork.com/ab/feed/jobs/rss?')
     if (!keyword || !rssLink) {
+      console.log('check')
       setIsEmptyFields((prevState) => !prevState)
+    } else if (!regex.test(rssLink)) {
+      setIsValid(false)
     } else {
       setKeyword('')
       setRssLink('')
+      setIsValid(true)
       await setLocalKeywords(keyword, rssLink)
       await setLocalJobs(keyword, rssLink)
     }
@@ -27,13 +33,19 @@ const AddKeyWordSection: React.FC = () => {
       submitHandler(keyword, rssLink)
     }
   }
+
+  const clearState = () => {
+    setIsEmptyFields(false)
+    setIsValid(true)
+  }
+
   return (
     <div className="flex justify-center flex-col gap-y-4">
       <div id="btn-group" className="gap-x-4 flex place-content-center">
         <input
           type="text"
           placeholder="Keyword"
-          onFocus={() => setIsEmptyFields(false)}
+          onFocus={() => clearState()}
           value={keyword}
           className={`bg-transparent border ${
             !isEmptyFields ? 'border-white' : 'border-red-600'
@@ -45,7 +57,7 @@ const AddKeyWordSection: React.FC = () => {
           type="text"
           placeholder="UpWork RSS Feed"
           value={rssLink}
-          onFocus={() => setIsEmptyFields(false)}
+          onFocus={() => clearState()}
           className={`bg-transparent border ${
             !isEmptyFields ? 'border-white' : 'border-red-600'
           } rounded-md px-4 py-2 text-lg`}
@@ -56,6 +68,7 @@ const AddKeyWordSection: React.FC = () => {
       {isEmptyFields && (
         <div className="text-red-600 text-md text-center">Please fill all the fields</div>
       )}
+      {!valid && <div className="text-red-600 text-md text-center">Enter a valid RSS Link</div>}
       <button
         onClick={() => submitHandler(keyword, rssLink)}
         className=" hover:text-gray-400 border w-2/5 mx-auto bg-transparent place-content-center border-white text-lg px-5 py-2 rounded-md"
