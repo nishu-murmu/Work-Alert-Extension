603acb85ff364b9583782d784eefb13476933f45
nishu-murmu <nishumurmu017@gmail.com>
2023-04-21 18:30:16 +0530

    fix: remove content scripts

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -1,11 +1,1 @@
-import MyComponent from './components/MyComponent'
-import ReactDOM from 'react-dom/client'
-
-let div = document.createElement('div')
-div.style.position = 'absolute'
-div.style.top = '100px'
-div.style.left = '100px'
-div.style.zIndex = '9999999'
-
-ReactDOM.createRoot(div).render(<MyComponent />)
-document.body.prepend(div)
+export {}
