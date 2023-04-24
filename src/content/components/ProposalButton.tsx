1861bd5a68bdc5e204c3ede95c3b80309b258f88
MadhuSaini22 <madhusaini46810@gmail.com>
2023-04-24 11:07:00 +0530

    create partial button

---
+++ src/content/components/ProposalButton.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function ProposalButton() {
+  return <button className="bg-red-600 py-4 px-8">Open</button>
+}
--- src/content/index.tsx
+++ src/content/index.tsx
@@ -1,1 +1,15 @@
-export {}
+import ProposalButton from './components/ProposalButton'
+import ReactDOM from 'react-dom/client'
+
+let div = document.createElement('div')
+div.style.position = 'absolute'
+div.style.bottom = '100px'
+div.style.right = '100px'
+div.style.zIndex = '9999999'
+div.style.backgroundColor = 'green'
+div.style.padding = '10px 15px'
+div.style.color = '#ffffff'
+div.style.cursor = 'pointer'
+
+ReactDOM.createRoot(div).render(<ProposalButton />)
+document.body.prepend(div)
