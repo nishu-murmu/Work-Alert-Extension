a43e474969d195f53723dfcb3b17da91c7adf448
nishu-murmu <nishumurmu017@gmail.com>
2023-04-25 14:23:12 +0530

    fix: fixing clear interval issue

--- src/content/components/ToggleSciprtButton.tsx
+++ src/content/components/ToggleSciprtButton.tsx
@@ -1,37 +1,42 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 
 const ToggleScriptButton: React.FC = () => {
-
-  let datafeed = document.querySelector('[data-test=tab-best-matches]') as HTMLElement
-  let mostRecent = document.querySelector('[data-test=tab-most-recent]') as HTMLElement
+  const intervalIdRef = useRef<any>(null)
   const [toggleScript, setToggleScript] = useState<boolean>(false)
-  
-  const sleep = () => {
-    return new Promise((resolve) => {
-      setTimeout(() => resolve(true), 100)
-    })
-  }
-  
+
   function toggleHandler() {
     setToggleScript((prevState) => !prevState)
-    toggleScriptHandler()
-  }
-  
-  function toggleScriptHandler() {
-    let intervalId = setInterval(async () => {
-      datafeed.click()
-  
-      await sleep()
-      mostRecent.click()
-    }, 10000)
-    if(!toggleScript) {
-      clearInterval(intervalId)
-    }
   }
 
+  function toggleScriptHandler() {
+    let datafeed = document.querySelector('[data-test=tab-best-matches]') as HTMLElement
+    let mostRecent = document.querySelector('[data-test=tab-most-recent]') as HTMLElement
+    const sleep = () => {
+      return new Promise((resolve) => {
+        setTimeout(() => resolve(true), 100)
+      })
+    }
+    intervalIdRef.current = setInterval(async () => {
+      datafeed.click()
+
+      await sleep()
+      mostRecent.click()
+    }, 5000)
+  }
+  function stopScriptHandler() {
+    clearInterval(intervalIdRef.current)
+  }
+
+  useEffect(() => {
+    toggleScript ? toggleScriptHandler() : stopScriptHandler()
+  }, [toggleScript])
+
   return (
-    <button onClick={() => toggleHandler()} className="text-black font-medium py-2 px-4 rounded-md hover:text-green-600">
-      Toggle Script {toggleScript ?"is On":"is Off"}
+    <button
+      onClick={() => toggleHandler()}
+      className="text-black font-medium py-2 px-4 rounded-md hover:text-green-600"
+    >
+      Toggle Script {toggleScript ? 'is On' : 'is Off'}
     </button>
   )
 }
