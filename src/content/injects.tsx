9b35ec46546d2bd8c5dd7f83a6364f86b388eb1e
nishu-murmu <nishumurmu017@gmail.com>
2023-04-26 11:41:47 +0530

    refactor: replacing div with li

--- src/content/injects.tsx
+++ src/content/injects.tsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client'
 import '../styles/content.css'
 import ToggleScriptButton from './components/ToggleSciprtButton'
 
-let listElem = document.createElement('div') as HTMLElement
+let listElem = document.createElement('li') as HTMLElement
 listElem.id = 'toggle-tabs'
 document.querySelector('.nav-messages')?.insertAdjacentElement('afterend', listElem)
 
