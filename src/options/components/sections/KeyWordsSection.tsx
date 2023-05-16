979320dd776d6d377e044e608fed485d782b17c8
MadhuSaini22 <madhusaini46810@gmail.com>
2023-05-16 13:18:22 +0530

    refact: home screen width

--- src/options/components/sections/KeyWordsSection.tsx
+++ src/options/components/sections/KeyWordsSection.tsx
@@ -2,13 +2,13 @@ import KeyWordCards from '../commonComponent/KeyWordCard'
 
 const KeyWordsSection: React.FC = () => {
   return (
-    <div className="w-[1500px]">
+    <div className="w-[1300px]">
       <div className="text-2xl flex justify-center gap-x-6">
         <div className="text-green-500 mt-3 font-bold">Keywords</div>
       </div>
       <div
         id="keywords"
-        className="w-4/5 flex items-center mx-auto overflow-y-hidden justify-center"
+        className="w-[90%] flex items-center mx-auto overflow-y-hidden justify-center"
       >
         <KeyWordCards />
       </div>
